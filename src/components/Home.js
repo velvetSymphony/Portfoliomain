24bd4e896f5e8d15010dec3a1f2b603361d59aec
Abhishekcs96 <79383816+Abhishekcs96@users.noreply.github.com>
2021-09-14 23:46:36 +0530

    removed unused dependencies, restyling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,64 +1,47 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
-import './styles/Home.css';
+import { Zoom, Fade, Bounce } from "react-awesome-reveal";
+import "./styles/Home.css";
 
 const Home = () => {
   return (
     <div className="space-top">
-    <motion.div
-      className="home"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1, duration: 2 }}
-    >
-      <motion.h1
-        className="head1"
-        initial={{ y: -300 }}
-        animate={{ y: 0 }}
-        transition={{ type: "spring", delay: 2, duration: 2 }}
-      >
-        Aspiring Dev full time
-      </motion.h1>
+      <h1 className="head1">
+        {" "}
+        <Zoom left>Aspiring Dev full time</Zoom>
+      </h1>
 
-      <motion.div
-        className="blockanimate"
-        initial={{ y: -350 }}
-        animate={{ y: 0 }}
-        transition={{ type: "spring", delay: 3, duration: 2 }}
-      >
-        <motion.h3
-          className="head3"
-          initial={{ y: -350 }}
-          animate={{ y: 0 }}
-          transition={{ type: "spring", delay: 3, duration: 2 }}
-        >
+      <h3 className="head3">
+        {" "}
+        <Bounce left>
           Welcome there!
-        </motion.h3>
+        </Bounce>
+      </h3>
 
-        <motion.p
-          className="para1"
-          initial={{ y: -400 }}
-          animate={{ y: 0 }}
-          transition={{ type: "spring", delay: 3, duration: 2 }}
-        >
+      <p className="para1">
+        {" "}
+        <Bounce left >
           My name is Abhishek and im your guest!
-        </motion.p>
-       <div className="link-wrap">
-        <motion.a className="Links" href="https://github.com/Abhishekcs96">
-          <FaGithubSquare />
-        </motion.a>
+        </Bounce>{" "}
+      </p>
 
-        <motion.a
+      <div className="link-wrap">
+        <a className="Links" href="https://github.com/Abhishekcs96">
+          <Bounce top >
+            <FaGithubSquare />
+          </Bounce>
+        </a>
+
+        <a
           className="Links"
           href="https://www.linkedin.com/in/abhishek-chandra-sekar-4018a8137/"
         >
-          <FaLinkedin />
-        </motion.a>
-        </div>
-      </motion.div>
-    </motion.div>
+          <Bounce top>
+            <FaLinkedin />
+          </Bounce>
+        </a>
+      </div>
     </div>
   );
 };
