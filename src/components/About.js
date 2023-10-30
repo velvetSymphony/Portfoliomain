d04aa3319664ab9ea5b6646b8e0603e256e2ed87
Abhishekcs96 <abhishekcs96@gmail.com>
2023-10-30 20:50:18 +1100

    nuke

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,3 @@
-import React from "react";
-import { Fade, Flip } from "react-reveal";
-import { DiJavascript1 } from "react-icons/di";
-import { SiTerraform } from "react-icons/si";
-import { FaPython } from "react-icons/fa";
-import { FaAws } from "react-icons/fa";
-import { FaLinux } from "react-icons/fa";
-import { FaDocker } from "react-icons/fa";
-import { FaGitAlt } from "react-icons/fa";
-import { FiGitlab } from "react-icons/fi";
-import "./styles/About.css";
-
-const About = () => {
-  return (
-    <div className="box-container" id="About">
-      <div className="content-container">
-        <Flip top cascade duration={500} triggeronce={true}>
-          <h1 className="About">About Myself</h1>
-          <div className="outline1"></div>
-        </Flip>
-        <div className="info-container">
-          <h3 id="name">Abhishek Chandra sekar</h3>
-          <h5 id="degree">
-            M.E.Sc Telecommunications, UNSW Australia, (2018-2020 Sept)
-          </h5>
-          <h5 id="degree">A little bit about myself</h5>
-          <p id="me-info">
             I&apos;m a Master&apos;s graduate from the University of New South
             Wales, Australia. A bit after graduation, I became interested in
             learning more about coding and front end dev technologies such as
@@ -39,49 +12,6 @@ const About = () => {
             all of the above and after all the time and now seeking a future in the area of
             IT(networks, cloud and devops), to which I am currently open to opportunities in.
           </p>
-        </div>
-
-        <div className="skills-container">
-          <Fade right triggerOnce={true} duration={2000}>
-            <h2>My Programming and IT Skills</h2>
-            <div className="skills-cover1">
-              <div className="skill">
-                <DiJavascript1 size={55} />
-                <p id="skill-desc">JavaScript</p>
-              </div>
-              <div className="skill">
-                <FaPython size={55} />
-                <p id="skill-desc">Python</p>
-              </div>
-              <div className="skill">
-                <SiTerraform size={55} />
-                <p id="skill-desc">Terraform</p>
-              </div>
-              <div className="skill">
-                <FaGitAlt size={55} />
-                <p id="skill-desc">Git</p>
-              </div>
-              <div className="skill">
-                <FiGitlab size={55} />
-                <p id="skill-desc">GitLab</p>
-              </div>
-              <div className="skill">
-                <FaAws size={55} />
-                <p id="skill-desc">AWS</p>
-              </div>
-              <div className="skill">
-                <FaDocker size={55} />
-                <p id="skill-desc">Docker</p>
-              </div>
-              <div className="skill">
-                <FaLinux size={55} />
-                <p id="skill-desc">Linux</p>
-              </div>
-            </div>
-          </Fade>
-        </div>
-        <div className="info-container">
-          <p id="me-info">
             I have a good foundational network background through my previous internship, 
             master&apos;s studies and I have worked on a basic level with
             Linux via oracle virtualbox (mainly using CentOS). Clearing my CCNA further improved my networking and IT knowledge. 
@@ -99,19 +29,3 @@ const About = () => {
             personal resources to the cloud. Currently, this website is hosted
             on AWS and the project&apos;s write-up can be read{" "}
             <a
-              id="writeup-link"
-              href="https://abhishekcs96.github.io"
-              target={"blank"}
-              referrerPolicy="no_refferer"
-            >
-              here
-            </a>
-            .
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default About;
--- src/components/Whatido.js
+++ src/components/Whatido.js
@@ -1,21 +1,4 @@
-import React from "react";
-import "./styles/Whatido.css";
-import { Fade } from "react-reveal";
-import CCNA from "./images/ccna.png";
-import AWS from "./images/aws-certified-solutions-architect-associate.png";
-import Cert1 from "./images/CERTIFICATE_LANDING_PAGE_GLLCYQLF8SR4.jpg";
-
-const Whatido = () => {
-  return (
-    <div className="Main-container" id="Certifications">
       <h3 className="whatdo">My Certifications</h3>
-      <div className="outline2"></div>
-      <ul className="items">
-        <Fade left triggerOnce={true} duration={1800}>
-          <li className="item-1">
-            <div className="icon-style">
-              <img
-                src={CCNA}
                 alt="Cisco Certified Network Associate"
                 id="image-center"
               ></img>
@@ -62,67 +45,3 @@ const Whatido = () => {
             </div>
           </li>
 
-          <li className="item-4">
-          <div className="icon-title1">Web Dev Certificates</div>
-          <div className="outline2"></div>
-            <div className="icon-style1">
-              <img
-                src={Cert1}
-                alt="Coursera Certificates"
-                id="image-center"
-              ></img>
-
-              <div className="icon-content">
-                
-                
-                <a
-                  className="icon-text1"
-                  href="https://coursera.org/share/6b68c5401bf6dcf410b1117258838e19"
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: "none" }}
-                >
-                  Interactivity with Javascript
-                </a>
-                <a
-                  className="icon-text1"
-                  href="https://coursera.org/share/3976a2b56deb70c72b588256ed7b5f8d"
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: "none" }}
-                >
-                  Introduction to CSS3
-                </a>
-                <a
-                  className="icon-text1"
-                  href="https://coursera.org/share/9d1c1761e92908eaf88de3db466e149a"
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: "none" }}
-                >
-                  Advanced Styling with Responsive Design
-                </a>
-                <a
-                  className="icon-text1"
-                  href="https://coursera.org/share/9c8d8eb0f51160f7b82f0720dea7b0e7"
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: "none" }}
-                >
-                  Introduction to HTML5
-                </a>
-                
-              </div>
-            </div>
-          </li>
-
-          
-        </Fade>
-      </ul>
-     
-
-    </div>
-  );
-};
-
-export default Whatido;
